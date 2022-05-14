6bf1d1f82be02ebb27b99edf5fad96b3fe6f835f
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2022-05-14 17:38:03 +0800

    bug fix

--- Sources/NavPush/NavPush.swift
+++ Sources/NavPush/NavPush.swift
@@ -9,19 +9,46 @@ public struct NavPush {
         self.uiViewController = uiViewController
         navigation = uiViewController.navigationController
     }
-
-    public func push<Content: View>(content: () -> (Content)) {
-        navigation?.pushViewController(UIHostingController(rootView: content()), animated: true)
-    }
-
     public func isLastView() -> Bool {
         return navigation?.viewControllers.last == uiViewController
+    }
+
+    public func dissmissAll() {
+        while navigation?.viewControllers.count ?? 0 > 1 {
+            navigation?.viewControllers.removeLast()
+        }
+    }
+
+    public func push<Content: View>(animated: Bool = true, _ view: () -> Content) {
+        let vc = UIHostingController(rootView: view())
+        navigation?.pushViewController(vc, animated: animated)
+    }
+
+    public func add<Content: View>(_ view: () -> Content) {
+        let vc = UIHostingController(rootView: view())
+        navigation?.viewControllers.append(vc)
+    }
+
+    public var count: Int {
+        navigation?.viewControllers.count ?? 0
+    }
+
+    static var navigations = [String : NavPush]()
+
+    public static subscript(_ name: String) -> NavPush? {
+        navigations[name]
     }
 }
 
 public extension View {
     func navPush(callBack: @escaping (NavPush) -> ()) -> some View {
         self.modifier(NavPushModifier(callBack: callBack))
+    }
+
+    func navPush(_ name: String) -> some View {
+        self.modifier(NavPushModifier {
+            NavPush.navigations[name] = $0
+        })
     }
 }
 
